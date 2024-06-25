5a39de28231a0fffa73b2fb373c9bf3a75bccb5f
Truman Hogg <hoggtruman@gmail.com>
2024-06-25 19:42:52 +0100

    Clean up Team DTOs

--- api/DTOs/Team/CreateTeamDTO.cs
+++ api/DTOs/Team/CreateTeamDTO.cs
@@ -7,7 +7,6 @@ public class CreateTeamDTO
 {
     public string TeamName { get; set; } = "";
 
-    // [MinLength(6, ErrorMessage = "A team must have 6 pokemon")]
-    [MaxLength(6, ErrorMessage = "A team must have 6 pokemon")]
+    [MaxLength(6, ErrorMessage = "A team can not have more than 6 pokemon")]
     public List<CreateUserPokemonDTO> UserPokemon { get; set; } = [];
 }
