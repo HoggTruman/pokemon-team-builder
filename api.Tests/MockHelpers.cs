721d16f4c2bf9d2cb768b1e56b4f931b8bfbaf97
Truman Hogg <hoggtruman@gmail.com>
2024-07-08 23:34:27 +0100

    Add AccountController tests

--- api.Tests/MockHelpers.cs
+++ api.Tests/MockHelpers.cs
@@ -1,4 +1,6 @@
 using api.Models.User;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -12,13 +14,16 @@ public static class MockHelpers
     public static Mock<UserManager<AppUser>> CreateMockUserManager() // IUserStore<AppUser> store = null
     {
         var store = new Mock<IUserStore<AppUser>>().Object; //store ??
+
         var options = new Mock<IOptions<IdentityOptions>>();
         var idOptions = new IdentityOptions();
         idOptions.Lockout.AllowedForNewUsers = false;
         options.Setup(o => o.Value).Returns(idOptions);
+
         var userValidators = new List<IUserValidator<AppUser>>();
         var validator = new Mock<IUserValidator<AppUser>>();
         userValidators.Add(validator.Object);
+
         var pwdValidators = new List<PasswordValidator<AppUser>>();
         pwdValidators.Add(new PasswordValidator<AppUser>());
 
@@ -38,5 +43,35 @@ public static class MockHelpers
         //     .Returns(Task.FromResult(IdentityResult.Success)).Verifiable();
 
         return mockUserManager;
+    }
+
+
+    public static Mock<SignInManager<AppUser>> CreateMockSignInManager()
+    {
+        var userManager = CreateMockUserManager().Object;
+        var contextAccessor = new Mock<IHttpContextAccessor>().Object;
+        var claimsFactory = new Mock<IUserClaimsPrincipalFactory<AppUser>>().Object;
+        
+        var options = new Mock<IOptions<IdentityOptions>>();
+        var idOptions = new IdentityOptions();
+        idOptions.Lockout.AllowedForNewUsers = false;
+        options.Setup(o => o.Value).Returns(idOptions);
+        
+        var logger = new Mock<ILogger<SignInManager<AppUser>>>().Object;
+        var schemes = new Mock<IAuthenticationSchemeProvider>().Object;
+        var confirmation = new Mock<IUserConfirmation<AppUser>>().Object;
+
+
+        var mockSignInManager = new Mock<SignInManager<AppUser>>(
+            userManager,
+            contextAccessor,
+            claimsFactory,
+            options.Object,
+            logger,
+            schemes,
+            confirmation
+        );
+
+        return mockSignInManager;
     }
 }
