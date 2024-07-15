3cf59adffa28a083f2669163f4fb14ba87d73380
Truman Hogg <hoggtruman@gmail.com>
2024-07-15 23:05:14 +0100

    Add tests for deletePokemonFromTeam

--- frontend/src/components/TeamEditPage/TeamEditMenu/TeamEditMenu.js
+++ frontend/src/components/TeamEditPage/TeamEditMenu/TeamEditMenu.js
@@ -29,6 +29,7 @@ function TeamEditMenu(props) {
         props.setActivePokemonSlot(newTeamSlot);
     }
 
+
     function handleClickDeletePokemonButton() {
         if (props.team.pokemon.length == 1) {
             return;
@@ -37,12 +38,6 @@ function TeamEditMenu(props) {
         props.setTeams(teams => {
             let team = teams.find(x => x.id == props.team.id);
             team = deletePokemonFromTeam(team, props.activePokemonSlot);
-            // team.pokemon = team.pokemon.filter(pokemon => pokemon.teamSlot != props.activePokemonSlot);
-            // for (const pokemon of team.pokemon) {
-            //     if (pokemon.teamSlot > props.activePokemonSlot) {
-            //         pokemon.teamSlot -= 1;
-            //     }
-            // }
 
             return [...teams]
         })
@@ -107,11 +102,26 @@ function TeamEditMenu(props) {
 }
 
 
+
+
 // Component Logic (Separated for testing)
-export function deletePokemonFromTeam(team, activePokemonSlot) {
-    team.pokemon = team.pokemon.filter(pokemon => pokemon.teamSlot != activePokemonSlot);
+
+/**
+ * 
+ * @param {object} team 
+ * @param {int} deleteSlot 
+ * @returns A shallow copy of the modified team object
+ * 
+ * Note: this modifies the team passed in
+ */
+export function deletePokemonFromTeam(team, deleteSlot) {
+    if (team.pokemon.length == 1) {
+        return team;
+    }
+
+    team.pokemon = team.pokemon.filter(pokemon => pokemon.teamSlot != deleteSlot);
     for (const pokemon of team.pokemon) {
-        if (pokemon.teamSlot > activePokemonSlot) {
+        if (pokemon.teamSlot > deleteSlot) {
             pokemon.teamSlot -= 1;
         }
     }
