5af63f33844d4973830c611a961b63e30c2afbe4
Truman Hogg <hoggtruman@gmail.com>
2024-07-30 16:05:44 +0100

    Fix missing ability effects in seed data

--- api/Mappers/CSVClassMaps/AbilityCSVMap.cs
+++ api/Mappers/CSVClassMaps/AbilityCSVMap.cs
@@ -9,6 +9,7 @@ namespace api.Mappers.CSVClassMaps
         {
             Map(m => m.Id).Index(0).Name("id");
             Map(m => m.Identifier).Index(1).Name("identifier");
+            Map(m => m.FlavorText).Index(1).Name("flavor_text");
         }
     }
 }
